1c3e7c46564ed361faaed192c8d1b039c1756335
that_girl <surrealisticshadow@gmail.com>
2016-01-15 15:46:30 -0800

    implemented listview to display linkedlist of days

--- app/src/main/java/com/example/that_girl/ketotrack/ViewDays.java
+++ app/src/main/java/com/example/that_girl/ketotrack/ViewDays.java
@@ -1,16 +1,80 @@
 package com.example.that_girl.ketotrack;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.LinkedList;
 
 public class ViewDays extends AppCompatActivity {
+
+    String DAYSFILE,USERFILE;
+    TextView textView;
+    File inputFile,inputFile1;
+    LinkedList<Day> days;
+    ListView listView;
+    ArrayAdapter adapter;
+    UserProfile user;
+
+    //Return to Main menu
+    public void backToMenu(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        ViewDays.this.startActivity(intent);
+    }
+
+    public void thisWorked(String s){
+        Context context = getApplicationContext();
+        CharSequence text = s;
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_days);
+        textView = (TextView) findViewById(R.id.textView);
+
+        DAYSFILE = getString(R.string.days_filename);
+        USERFILE = getString(R.string.user_filename);
+        inputFile = new File (getFilesDir(),DAYSFILE);
+        inputFile1 = new File (getFilesDir(),USERFILE);
+
+        try{
+            inputFile = new File(getFilesDir(),DAYSFILE);
+            FileInputStream fis = new FileInputStream(inputFile);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            days = (LinkedList) ois.readObject();
+            ois.close();
+
+            listView = (ListView) findViewById(R.id.listView1);
+            adapter = new ArrayAdapter(this,R.layout.item_list_appearance,days);
+            listView.setAdapter(adapter);
+
+
+            FileInputStream fis1 = new FileInputStream(inputFile1);
+            ObjectInputStream ois1 = new ObjectInputStream(fis1);
+            user = (UserProfile) ois1.readObject();
+            ois1.close();
+            thisWorked(user.toString());
+
+            textView.setText(user.toString());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
